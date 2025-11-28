19607c90be51701a4c75375d30563aaea2c72a3a
sinueh-andreata <sinuehnoan@gmail.com>
2025-11-28 16:16:50 -0300

    feat: jwt authentication and authorization added in admin endpoint

--- src/controllers/admin_controller.py
+++ src/controllers/admin_controller.py
@@ -4,16 +4,27 @@ from src.services.products_service import ProductsService
 from src.extensions import db
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from src.auth.jwt_auth import roles_required
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 @admin_bp.route('/create/products', methods=['POST'])
+@jwt_required()
+@roles_required('admin')
 def create_product():
     schema = ProductSchema()
     try:
         dados = schema.load(request.get_json())
-        service = ProductsService(db.session, current_user)
+        user_id = get_jwt_identity()
+        service = ProductsService(db.session, user_id)
         product = service.save_product(dados)
         return jsonify(schema.dump(product)), 201
-    except Exception as err:
-        return jsonify({"error": 'erro interno do servidor'}), 500
+    except ValidationError as err:
+        return jsonify({"error": err.messages}), 400
+    except IntegrityError:
+        return jsonify({"error": "Produto já existe"}), 409
+    except Exception:
+        return jsonify({"error": "Erro interno do servidor"}), 500
+
--- src/services/products_service.py
+++ src/services/products_service.py
@@ -1,15 +1,13 @@
 from src.models.models import Product
-from flask import jsonify
-from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
 
 class ProductsService:
-    def __init__(self, db_session, current_user):
+    def __init__(self, db_session, user_id: int):
         self.db_session = db_session
-        self.current_user = current_user
-    
+        self.user_id = user_id
+
     def create_product(self, dados):
-        return Product(**dados, user_id=self.current_user.id)
+        return Product(**dados, user_id=self.user_id)
 
     def save_product(self, dados):
         product = self.create_product(dados)
@@ -17,52 +15,51 @@ class ProductsService:
             self.db_session.add(product)
             self.db_session.commit()
             return product
-        except ValidationError as err:
-            return jsonify({"errors": err.messages}), 400
-        except IntegrityError as err:
+        except IntegrityError:
             self.db_session.rollback()
-            return jsonify({"error": "Erro de integridade ao cadastrar o product."}), 409
-        except Exception as err:
-            return jsonify({"error": 'erro interno do servidor'}), 500
-        
+            raise  # deixa o controller tratar o erro
+        except Exception:
+            self.db_session.rollback()
+            raise
+
     def list_all_products(self):
         return Product.query.all()
-    
+
     def get_product(self, id):
         return self.db_session.query(Product).filter_by(id=id).first()
-    
+
     def update_product(self, dados, id):
         product = self.db_session.query(Product).filter_by(
             id=id,
-            user_id=self.current_user.id
+            user_id=self.user_id
         ).first()
-        
+
         if not product:
             return None
-        
+
         for key, value in dados.items():
             setattr(product, key, value)
-            
+
         try:
             self.db_session.commit()
-        except Exception as e:
+            return product
+        except Exception:
             self.db_session.rollback()
-            raise e
-        return product
-    
+            raise
+
     def delete_product(self, id):
         product = self.db_session.query(Product).filter_by(
             id=id,
-            user_id=self.current_user.id
+            user_id=self.user_id
         ).first()
-        
+
         if not product:
             return None
-        
+
         try:
             self.db_session.delete(product)
             self.db_session.commit()
             return product
-        except Exception as e:
+        except Exception:
             self.db_session.rollback()
-            raise e
+            raise
